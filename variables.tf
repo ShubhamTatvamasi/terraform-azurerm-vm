c66a30a6c254a8216a381385335dee5b186f28e5
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2025-11-03 10:08:37 +0530

    modified:   variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,25 @@
 variable "prefix" {
-  default = "demo"
+  description = "Prefix for all resources."
+  type        = string
+  default     = "demo"
 }
 
 variable "location" {
-  default = "Central India"
+  description = "The Azure region to deploy resources into."
+  type        = string
+  default     = "Central India"
 }
 
 variable "vm_size" {
-  default = "Standard_B1s"
+  description = "The size of the virtual machine."
+  type        = string
+  default     = "Standard_B1s"
 }
 
 variable "ssh_public_key" {
-  default = "~/.ssh/id_ed25519.pub"
+  description = "Path to the SSH public key."
+  type        = string
+  default     = "~/.ssh/id_ed25519.pub"
 }
 
 variable "availability_zone" {
