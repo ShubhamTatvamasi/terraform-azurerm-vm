2cfc1508e5338668c676165a16e141fe42571fa8
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2025-11-08 16:30:05 +0530

    modified:   linux_virtual_machine.tf

--- linux_virtual_machine.tf
+++ linux_virtual_machine.tf
@@ -16,7 +16,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
+    storage_account_type = "StandardSSD_LRS"
   }
 
   source_image_reference {
